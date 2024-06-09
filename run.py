7b4e5701ad1f5a1052b08b46e72412383dfe74f9
Michael McCourt <mmccourt928@outlook.com>
2024-06-09 12:46:24 +0000

    cred.json re-added to gitignore, run.py updated with relevant file path

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
-CREDS = Credentials.from_service_account_file('p3-template/cred.json')
+CREDS = Credentials.from_service_account_file('cred.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches_proj')
